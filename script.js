0f64e43236aef996304e0d1b3d0197233cd47906
Mohamed Elfwakhry <90508774+MohamedElfwakhry@users.noreply.github.com>
2022-01-23 13:18:55 +0200

    -m
    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu').addEventListener('click',()=>{
+  document.querySelector('.container').classList.toggle("change");
+  document.querySelector('.sidebar-container').classList.toggle("change");
+})
