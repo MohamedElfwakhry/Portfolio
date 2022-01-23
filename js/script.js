2e1f6ba203a2b14519fb929d969b488fdbe73e07
Mohamed Elfwakhry <90508774+MohamedElfwakhry@users.noreply.github.com>
2022-01-23 13:20:26 +0200

    -m
    dd

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-icon').addEventListener('click',()=>{
+  document.querySelector('.parent').classList.toggle("change");
+});
+
+// function myfunction(li) {
+//     var TextInsideLi = li.getElementsByTagName('p')[0];
+//
+//     if (TextInsideLi.style.display !== "block") {
+//         TextInsideLi.style.display = "block";
+//       } else {
+//         TextInsideLi.style.display = "none";
+//       }
+//
+// }
